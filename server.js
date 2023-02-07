445add1fa4185c0ace36cb7081cdf9a0654d8802
MasakiDesktop <mandij50@gmail.com>
2023-02-07 09:59:58 -0500

    Hope so

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const {v4:uuid} = require('uuid');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const db = path.join(__dirname, 'db', 'db.json')
 let notes = JSON.parse(fs.readFileSync(db, {encoding: 'utf-8'}));
 
